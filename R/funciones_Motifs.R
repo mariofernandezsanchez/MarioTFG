8ea12f83ef6789b947cbbc4d16733c2158ffb0cc
mariofernandezsanchez <mario.f.s@um.es>
2022-11-01 20:34:14 +0100

    More Functions

--- R/funciones_Motifs.R
+++ R/funciones_Motifs.R
@@ -1,10 +1,13 @@
 #' @title Convert a motif list in format ".fasta" to DNAStringSet
-#' @description Function that helps us convert ...
-#'
-#' @param motif Motif we want to see
-#' @return Motif in format data.frame
+#' @description Function that helps us to convert a background sequence
+#' in a fasta format to one in DNAStringSet format
+#' @param ourpath Path where the sequence will be found
+#' @param nFlist The file .fasta (number)
+#' @return The DNAStringSet file
 #' @note
-#' listToDF_Motifs(motif)
+#'
+#' file<-convertToDNAStringSet=function("ourpath", nFlist)
+#'
 #' @export
 convertToDNAStringSet=function(ourpath, nFlist){
 
@@ -12,12 +15,13 @@ convertToDNAStringSet=function(ourpath, nFlist){
   file_seq <- (utils::read.table(list_files[nFlist]))
   file_seq <- file_seq[!BiocGenerics::grepl("^>", file_seq$V1),]
   file_seq <- BiocGenerics::as.vector(file_seq)
-  seq_file <- Biostrings::DNAStringSet(file_seq)
+  seq_file2 <- Biostrings::DNAStringSet(file_seq)
 }
 
 #' @title Compare motifs (list)
 #' @description Function that compares all motifs to all other motifs and
-#' helps us to observe which are the motifs that are are closer to each other at the sequence level.
+#' helps us to observe which are the motifs that are are closer to each other
+#' at the sequence level.
 #' @param motif Motif in formar list
 #' @param type The type of matrix that represents the motif and that will be
 #' used for alignment. You can chose between "PPM" or "ICM"
@@ -25,11 +29,12 @@ convertToDNAStringSet=function(ourpath, nFlist){
 #' EUCL,SW,KL,ALLR,BHAT,HELL,SEUCL,MAN,ALLR_LL, WEUCL, WPCC. See details
 #' @return Motif alignment scoring matrix
 #' @note
-#'  compare_Motifs(motif, "type", "method") Dont't forget use the quotes in type and method
+#' compare_Motifs(motif, "type", "method") Dont't forget use the quotes
 #'
 #' @export
 compare_Motifs=function(motif, type, method){
-  universalmotif::compare_motifs(motif, use.type = type, method = method, tryRC = F)
+  universalmotif::compare_motifs(motif, use.type = type, method = method,
+  tryRC = F)
 }
 
 #' @title Create motifs
@@ -83,15 +88,15 @@ create_Motifeval=function(evall){
   universalmotif::create_motif(eval = evall)
 }
 
-#' @title Enrich motif ... TODO
-#' @description Function that ... TODO
-#' @param motifs Motifs we use .. . TODO
-#' @param seq Sequence we use .. . TODO
-#' @param qval Sequence we use .. . TODO
-#' @return The motif TODO
+#' @title Enrich motifs
+#' @description Function that helps us to make an enrichment analysis
+#' of the refined motif compared to its background sequence.
+#' @param motifs The motif to analyze
+#' @param seq The background sequence
+#' @return The enrichment analysis of this motif
 #' @note
-#' enrich...
 #'
+#' enrich_Motifs=function(cdk2_100,seq_file_cdk2_100)
 #' @export
 enrich_Motifs=function(motif,seq){
   universalmotif::enrich_motifs(motifs = motif, sequences = seq, tryRC = T,
@@ -113,14 +118,14 @@ listToDF_Motifs=function(motif){
   universalmotif::summarise_motifs(motif)
 }
 
-#' @title Merge motifs ... TODO funciona bien
-#' @description Function that helps ... TODO
-#' @param motifs List of motif we want to merge
-#' @return The motifs merged
+#' @title Merge motifs
+#' @description Function that helps us to merge all the motifs into one,
+#' to obtain the final motif.
+#' @param motifs List of motifs we want to merge
+#' @return The definitive motif
 #' @note
 #'
 #' merge.motifs <- merge_Motifs=function(motif)
-#' view_motifs(merge_motif(m1, m2, m3))
 #'
 #' @export
 merge_Motifs=function(motifs){
@@ -131,28 +136,28 @@ merge_Motifs=function(motifs){
 
 #' @title Import motifs in format HOMER
 #' @description Function that loading y makes a motif understandable to humans
-#' @param rutadelmotiv Path in our pc of the motif that we want to import
+#' @param ourpath Path in our pc of the motif that we want to import
 #' @return The motif list with the parameters more importants
 #' @note
-#' read_Motifs(rutadelmotiv = "./example/example.motif")
+#' read_Motifs(ourpath = "./example/example.motif")
 #'
 #' @export
-read_Motifs=function(rutadelmotiv){
-  universalmotif::read_homer(rutadelmotiv)
+read_Motifs=function(ourpath){
+  universalmotif::read_homer(ourpath)
 }
 
 #' @title Get a heatmap of the selected motifs (list)
-#' @description Function that by means of a graphical plot allows us to visualize the
-# 'matrix obtained in the comparison of motifs, and that helps us to select the
-# 'cluster that we are looking for.
-#' @param motif List of motifs
-#' @return Motif alignment scoring matrix
+#' @description Function that by means of a graphical plot allows us
+#' to visualize the matrix obtained in the comparison of motifs, and that
+#' helps us to select the cluster that we are looking for.
+#' @param matrix Alignment scoring matrix
+#' @return Heatmap of the clouter motifs
 #' @note
-#'  heatMap_Motifs(motif)
+#'  heatMap_Motifs(compare_Motifs(motif = xxx,"xxx","xxx"))
 #'
 #' @export
-heatMap_Motifs=function(motif){
-  heatmaply::ggheatmap(motif)
+heatMap_Motifs=function(matrix){
+  heatmaply::ggheatmap(matrix)
 }
 
 #' @title Score of all motifs in a sequence (data.frame)
@@ -180,7 +185,8 @@ scoreSeq_Motifs=function(motif){
 #' @description Function that gets the punctuation of a motif specified by
 #' parameter according to the mathematical formula of (Almagro-Hernández and
 #' Fernández-Breis, 2020)
-#' @param motif The motif in data.frame format from which you want to obtain the scores
+#' @param motif The motif in data.frame format from which you want to obtain the
+#' scores
 #' @param x Motif number within the sequence
 #' @return The score result for that motif
 #' @note
@@ -192,13 +198,20 @@ score_Motif=function(motif, x){
 }
 
 #' @title Scan sequences for matches to input motifs
-#' @description TODO ...
-#' @param motif The motif in data.frame format from which you want to obtain the scores
-#' @param seq TODO ...
-#' @param th TODO ...
-#' @return TODO ...
+#' @description This function is used to scan the motif of each set
+#' in its respective set of peaks sequences, in order to obtain the subsequences
+#' of the same length as the motif, that exceed the established threshold.
+#' If that score exceeds the predetermined threshold, it is considered that
+#' there is a match and that subsequence is one of those that the function
+#' returns and will be used to generate the refined motif.
+#' @param motif The motif in correct format
+#' @param seq Sequence obtained in step 1 of each experiment
+#' in DNAStringSet format
+#' @param th Threshold that determines whether or not a sequence contains the
+#' motif
+#' @return The analysis of scanned sequences
 #' @note
-#' score_Motif(motifdf, x)
+#' scan_Motif(motif,seq_file,0,70)
 #'
 #' @export
 scan_Motif=function(motif,seq,th){
@@ -208,7 +221,8 @@ scan_Motif=function(motif,seq,th){
 }
 
 #' @title Draw a plot with the indicated motif
-#' @description Function that draws a graphic of the motif that is indicated as a parameter
+#' @description Function that draws a graphic of the motif that is indicated as
+#' a parameter
 #' @param motif Motif we want to see
 #' @param n Number of the motif within the sequence
 #' @return The graphic with the motif sequence logo
@@ -222,7 +236,7 @@ see_Motif=function(motif,n){
 }
 
 
-#' @title Trim motifs
+#' @title Trim the motifs
 #' @description Function that Eliminates the positions of those motifs that
 #' contain little information
 #' @param motif Motif already merged
@@ -234,3 +248,17 @@ see_Motif=function(motif,n){
 trim_Motifs=function(motif){
   universalmotif::trim_motifs(motifs = motif)
 }
+
+#' @title Export motifs to jaspar file
+#' @description Function than
+#' @param motif Motif already merged
+#' @param filename Name of the file
+#' @return The definitive motif
+#' @note
+#' export_Jaspar(motifxxx,"file.txt") *don´t forget the ""
+#'
+#' @export
+export_Jaspar=function(motif, filename){
+  universalmotif::write_jaspar(motif,filename)
+}
+
